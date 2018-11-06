622d1c76d11b74192d9edf1c7fd762be7a88515c
katai5plate <katai5plate.sub@gmail.com>
2018-11-07 05:46:36 +0900

    delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http'),
-  src = require('fs').readFileSync("./src/test.html", {
-    encoding: 'utf8'
-  })
-server = http.createServer((_, res) => res.end(src));
-server.listen(3000, () => {
-  console.log('http://localhost:3000/')
-})
